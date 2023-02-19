c88d48700d4e6b7f1f814c1ff8c9ff24ae603cf9
Spectrum5 <toskusergio@gmail.com>
2023-02-19 23:56:32 +0100

    Esercizio finito. - da aggiungere style

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
+<?php
+$text = "Im not afraid to take a stand
+
+Everybody come take my hand
+
+Well walk this road together, through the storm
+
+Whatever weather, cold or warm
+
+Just let you know that, youre not alone
+
+Hola if you feel that youve been down the same road";
+$word = $_GET['word'];
+$censure = str_replace($word, '***', $text);
+?>
+
 <!DOCTYPE html>
 <html lang="it">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>PHP Badwords</title>
-    </head>
 
-    <body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
 
-    </body>
+<body>
+    <h1> <?php echo $censure . '<br>' . " Il Paragrafo è composto da: " .  strlen($censure) . ' caratteri' ?> </h1>
+</body>
+
 </html>
